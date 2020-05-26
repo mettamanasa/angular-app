5e719c4a7703ec48dcebda3f305173cd04e813a5
Manasa <manasa.metta15@gmail.com>
2020-05-26 15:06:11 +0530

    angular application from scratch

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,30 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { RecipeService } from './../recipe.service';
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [];
+
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+  // onRecipeSelected(recipe: Recipe) {
+  //   this.recipeWasSelected.emit(recipe);
+  // }
+
+}
